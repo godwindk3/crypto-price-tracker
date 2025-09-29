8fc999a10c50b9514dcc2f31eee9a690f63c1db0
godwindk3 <phongprosupper@gmail.com>
2025-09-29 10:09:20 +0700

    first api key test mtfk

--- src/api_key_test.py
+++ src/api_key_test.py
@@ -1,1 +1,21 @@
-print("Hello world")
+from dotenv import load_dotenv
+import os
+import requests
+
+gecko_api_key = os.getenv("GECKO_API_KEY")
+
+url = "https://api.coingecko.com/api/v3/simple/price"
+
+params = {
+    "ids": "bitcoin,ethereum",
+    "vs_currencies": "usd"
+}
+
+headers = {
+    "acccept": "application/json",
+    "x-cg-pro-api-key": gecko_api_key
+}
+
+respond = requests.get(url, params=params, headers=headers)
+
+print(respond.json())
